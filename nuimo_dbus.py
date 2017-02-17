32b05d1d077853494ec4dcdcce59409b0e7a2dc3
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-02-17 16:45:07 +0100

    Clean code: reorder classes by granularity level

--- nuimo_dbus.py
+++ nuimo_dbus.py
@@ -7,40 +7,100 @@ from enum import Enum
 from gi.repository import GObject
 
 
-class Gesture(Enum):
-    BUTTON_PRESS = 1
-    BUTTON_RELEASE = 2
-    SWIPE_LEFT = 3
-    SWIPE_RIGHT = 4
-    SWIPE_UP = 5
-    SWIPE_DOWN = 6
-    TOUCH_LEFT = 8,
-    TOUCH_RIGHT = 9,
-    TOUCH_TOP = 10,
-    TOUCH_BOTTOM = 11,
-    LONGTOUCH_LEFT = 12
-    LONGTOUCH_RIGHT = 13
-    LONGTOUCH_TOP = 14,
-    LONGTOUCH_BOTTOM = 15,
-    ROTATION = 16,
-    FLY_LEFT = 17,
-    FLY_RIGHT = 18,
-    FLY_UPDOWN = 19
+# TODO: Extract reusable `DeviceDiscovery` class
+class ControllerManager:
+    def __init__(self, adapter_name='hci0'):
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        self.mainloop = GObject.MainLoop()
+        self.listener = None
+        self.bus = dbus.SystemBus()
+        self.object_manager = dbus.Interface(
+            self.bus.get_object('org.bluez', '/'),
+            'org.freedesktop.DBus.ObjectManager')
+
+        # TODO: Get adapter from managed objects? See bluezutils.py
+        adapter_object = self.bus.get_object('org.bluez', '/org/bluez/' + adapter_name)
+        self.adapter = dbus.Interface(adapter_object, 'org.bluez.Adapter1')
+
+        self.adapter_name = adapter_name
+        self.device_path_regex = re.compile('^/org/bluez/' + adapter_name + '/dev((_[A-Z0-9]{2}){6})$')
+
+        self._interface_added_signal = None
+        self._properties_changed_signal = None
+
+    def run(self):
+        """Starts the main loop that is necessary to receive Bluetooth events from the Bluetooth driver.
+           This call blocks until you call `stop()` to stop the main loop."""
+        self.mainloop.run()
+
+    def stop(self):
+        """Stops the main loop started with `start()`"""
+        self.mainloop.quit()
+
+    def known_controllers(self):
+        # TODO: Return known devices
+        # see https://github.com/bbirand/python-dbus-gatt/blob/master/discovery.py
+        return []
+
+    def start_discovery(self):
+        # TODO: Support service UUID filter
+        # see http://git.kernel.org/cgit/bluetooth/bluez.git/tree/doc/adapter-api.txt#n57
+        self._discovered_controllers = {}
+
+        self._interface_added_signal = self.bus.add_signal_receiver(
+            self._interfaces_added,
+            dbus_interface='org.freedesktop.DBus.ObjectManager',
+            signal_name='InterfacesAdded')
+
+        self._properties_changed_signal = self.bus.add_signal_receiver(
+            self._properties_changed,
+            dbus_interface=dbus.PROPERTIES_IFACE,
+            signal_name='PropertiesChanged',
+            arg0='org.bluez.Device1',
+            path_keyword='path')
+
+        self.adapter.SetDiscoveryFilter({
+            'UUIDs': Controller.SERVICE_UUIDS,
+            'Transport': 'le'})
+        self.adapter.StartDiscovery()
+
+    def stop_discovery(self):
+        if self._interface_added_signal is not None:
+            self._interface_added_signal.remove()
+        if self._properties_changed_signal is not None:
+            self._properties_changed_signal.remove()
+        self.adapter.StopDiscovery()
+
+    def _interfaces_added(self, path, interfaces):
+        self._device_discovered(path, interfaces)
+
+    def _properties_changed(self, interface, changed, invalidated, path):
+        # TODO: Handle `changed` and `invalidated` properties and update device
+        self._device_discovered(path, [interface])
+
+    def _device_discovered(self, path, interfaces):
+        if 'org.bluez.Device1' not in interfaces:
+            return
+        match = self.device_path_regex.match(path)
+        if not match:
+            return
+        mac_address = match.group(1)[1:].replace('_', ':').lower()
+        alias = gatt.Device(adapter_name=self.adapter_name, mac_address=mac_address).alias()
+        if alias != 'Nuimo':
+            return
+        controller = Controller(adapter_name=self.adapter_name, mac_address=mac_address)
+        discovered_controller = self._discovered_controllers.get(controller.mac_address, None)
+        if discovered_controller is None:
+            self._discovered_controllers[mac_address] = controller
+            if self.listener is not None:
+                self.listener.controller_discovered(controller)
+        else:
+            discovered_controller.advertised()
 
 
-class GestureEvent:
-    def __init__(self, gesture, value=None):
-        self.gesture = gesture
-        self.value = value
-
-    def __repr__(self):
-        return str(self.gesture) + (("," + str(self.value)) if self.value is not None else "")
-
-
-class LedMatrix:
-    def __init__(self, string):
-        string = '{:<81}'.format(string[:81])
-        self.leds = [c not in [' ', '0'] for c in string]
+class ControllerManagerListener:
+    def controller_discovered(self, controller):
+        pass
 
 
 class Controller(gatt.Device):
@@ -210,102 +270,6 @@ class ControllerListener:
         pass
 
 
-class ControllerManagerListener:
-    def controller_discovered(self, controller):
-        pass
-
-
-# TODO: Extract reusable `DeviceDiscovery` class
-class ControllerManager:
-    def __init__(self, adapter_name='hci0'):
-        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-        self.mainloop = GObject.MainLoop()
-        self.listener = None
-        self.bus = dbus.SystemBus()
-        self.object_manager = dbus.Interface(
-            self.bus.get_object('org.bluez', '/'),
-            'org.freedesktop.DBus.ObjectManager')
-
-        # TODO: Get adapter from managed objects? See bluezutils.py
-        adapter_object = self.bus.get_object('org.bluez', '/org/bluez/' + adapter_name)
-        self.adapter = dbus.Interface(adapter_object, 'org.bluez.Adapter1')
-
-        self.adapter_name = adapter_name
-        self.device_path_regex = re.compile('^/org/bluez/' + adapter_name + '/dev((_[A-Z0-9]{2}){6})$')
-
-        self._interface_added_signal = None
-        self._properties_changed_signal = None
-
-    def run(self):
-        """Starts the main loop that is necessary to receive Bluetooth events from the Bluetooth driver.
-           This call blocks until you call `stop()` to stop the main loop."""
-        self.mainloop.run()
-
-    def stop(self):
-        """Stops the main loop started with `start()`"""
-        self.mainloop.quit()
-
-    def known_controllers(self):
-        # TODO: Return known devices
-        # see https://github.com/bbirand/python-dbus-gatt/blob/master/discovery.py
-        return []
-
-    def start_discovery(self):
-        # TODO: Support service UUID filter
-        # see http://git.kernel.org/cgit/bluetooth/bluez.git/tree/doc/adapter-api.txt#n57
-        self._discovered_controllers = {}
-
-        self._interface_added_signal = self.bus.add_signal_receiver(
-            self._interfaces_added,
-            dbus_interface='org.freedesktop.DBus.ObjectManager',
-            signal_name='InterfacesAdded')
-
-        self._properties_changed_signal = self.bus.add_signal_receiver(
-            self._properties_changed,
-            dbus_interface=dbus.PROPERTIES_IFACE,
-            signal_name='PropertiesChanged',
-            arg0='org.bluez.Device1',
-            path_keyword='path')
-
-        self.adapter.SetDiscoveryFilter({
-            'UUIDs': Controller.SERVICE_UUIDS,
-            'Transport': 'le'})
-        self.adapter.StartDiscovery()
-
-    def stop_discovery(self):
-        if self._interface_added_signal is not None:
-            self._interface_added_signal.remove()
-        if self._properties_changed_signal is not None:
-            self._properties_changed_signal.remove()
-        self.adapter.StopDiscovery()
-
-    def _interfaces_added(self, path, interfaces):
-        self._device_discovered(path, interfaces)
-
-    def _properties_changed(self, interface, changed, invalidated, path):
-        # TODO: Handle `changed` and `invalidated` properties and update device
-        self._device_discovered(path, [interface])
-
-    def _device_discovered(self, path, interfaces):
-        if 'org.bluez.Device1' not in interfaces:
-            return
-        match = self.device_path_regex.match(path)
-        if not match:
-            return
-        mac_address = match.group(1)[1:].replace('_', ':').lower()
-        alias = gatt.Device(adapter_name=self.adapter_name, mac_address=mac_address).alias()
-        if alias != 'Nuimo':
-            return
-        controller = Controller(adapter_name=self.adapter_name, mac_address=mac_address)
-        discovered_controller = self._discovered_controllers.get(controller.mac_address, None)
-        if discovered_controller is None:
-            self._discovered_controllers[mac_address] = controller
-            if self.listener is not None:
-                self.listener.controller_discovered(controller)
-        else:
-            discovered_controller.advertised()
-
-
 class ControllerPrintListener(ControllerListener):
     def __init__(self, controller):
         self.controller = controller
@@ -330,3 +294,39 @@ class ControllerPrintListener(ControllerListener):
 
     def print(self, string):
         print("Nuimo controller " + self.controller.mac_address + " " + string)
+
+
+class GestureEvent:
+    def __init__(self, gesture, value=None):
+        self.gesture = gesture
+        self.value = value
+
+    def __repr__(self):
+        return str(self.gesture) + (("," + str(self.value)) if self.value is not None else "")
+
+
+class Gesture(Enum):
+    BUTTON_PRESS = 1
+    BUTTON_RELEASE = 2
+    SWIPE_LEFT = 3
+    SWIPE_RIGHT = 4
+    SWIPE_UP = 5
+    SWIPE_DOWN = 6
+    TOUCH_LEFT = 8,
+    TOUCH_RIGHT = 9,
+    TOUCH_TOP = 10,
+    TOUCH_BOTTOM = 11,
+    LONGTOUCH_LEFT = 12
+    LONGTOUCH_RIGHT = 13
+    LONGTOUCH_TOP = 14,
+    LONGTOUCH_BOTTOM = 15,
+    ROTATION = 16,
+    FLY_LEFT = 17,
+    FLY_RIGHT = 18,
+    FLY_UPDOWN = 19
+
+
+class LedMatrix:
+    def __init__(self, string):
+        string = '{:<81}'.format(string[:81])
+        self.leds = [c not in [' ', '0'] for c in string]
