0d48e04f2d5db7ed62d3120cf14e1c47a4ac081b
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-02-20 17:57:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='nuimo',
-      version='0.1.0',
-      description='Nuimo SDK for Python on Linux',
-      url='https://github.com/getsenic/nuimo-linux-python',
-      maintainer='Senic GmbH',
-      maintainer_email='developers@senic.com',
-      license='MIT',
-      install_requires=['gattlib'],
-      py_modules=['nuimo'])
+setup(
+	name='nuimo',
+    version='0.1.0',
+    description='Nuimo SDK for Python on Linux',
+    url='https://github.com/getsenic/nuimo-linux-python',
+    maintainer='Senic GmbH',
+    maintainer_email='developers@senic.com',
+    license='MIT',
+    install_requires=['dbus-python'],
+    py_modules=['nuimo'])
