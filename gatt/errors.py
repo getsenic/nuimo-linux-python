840afd11dc7b767d8309791368dc0075094be9f6
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-02-22 13:49:23 +0100

    Clean code

--- gatt/errors.py
+++ gatt/errors.py
@@ -1,14 +1,6 @@
 class InProgress(Exception):
-	def __rep__(self):
-		return "inprogress"
-
-	def __str__(self):
-		return "inprogress"
+        pass
 
 
 class Unknown(Exception):
-	def __rep__(self):
-		return "unknwown"
-
-	def __str__(self):
-		return "unknwown"
+        pass
