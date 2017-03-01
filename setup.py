bc597e1ba07f776041cefa808bab857da42e7db7
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-01 14:32:36 +0100

    Require gatt 0.1.1

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='nuimo',
     packages=['nuimo'],
-    install_requires=['gatt==0.1.0'],
-    version='0.2.0',
+    install_requires=['gatt==0.1.1'],
+    version='0.2.1',
     description='Nuimo SDK for Python on Linux',
     keywords='nuimo',
     url='https://github.com/getsenic/nuimo-linux-python',
