2433375ae34ef369dd2b4eadc1cddcd9c2d4efde
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-07 14:39:02 +0100

    Require gatt version >= 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='nuimo',
     packages=['nuimo'],
-    install_requires=['gatt>=0.1.2'],
+    install_requires=['gatt>=0.1.3'],
     version='0.2.2',
     description='Nuimo SDK for Python on Linux',
     keywords='nuimo',
