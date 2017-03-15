788b468746371d5ac24dd1087afa0dd919d00d4b
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-15 15:47:33 +0100

    Set version to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='nuimo',
     packages=['nuimo'],
     install_requires=['gatt>=0.2.0'],
-    version='0.2.4',
+    version='0.3.0',
     description='Nuimo SDK for Python on Linux',
     keywords='nuimo',
     url='https://github.com/getsenic/nuimo-linux-python',
